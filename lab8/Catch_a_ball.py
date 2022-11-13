332747b42b2e487d6e45e6f114aea46567398048
Макаров Артём <info4u2004@mail.ru>
2022-11-13 13:36:40 +0300

    Unworking version - trying to add second ball.

--- lab8/Catch_a_ball.py
+++ lab8/Catch_a_ball.py
@@ -19,42 +19,52 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 global x, y, r, color, speedx, speedy, lifetime, countertime
 
-def new_ball():
-    global x, y, r, color, speedx, speedy, lifetime, countertime
-    x = randint(100, 700)
-    y = randint(100, 500)
-    r = randint(30, 50)
-    color = COLORS[randint(0, 5)]
-    speedx = randint(-8, 8)
-    speedy = randint(-8, 8)
-    lifetime = randint(25, 100)
-    countertime = 0
-    circle(screen, color, (x, y), r)
+x = [0]*2
+y = [0]*2
+r = [0]*2
+color = [0]*2
+speedx = [0]*2
+speedy = [0]*2
+lifetime = [0]*2
+countertime = [0]*2
 
-def update_position():
+def new_ball(i):
+    global x, y, r, color, speedx, speedy, lifetime, countertime
+    x[i] = randint(100, 700)
+    y[i] = randint(100, 500)
+    r[i] = randint(30, 50)
+    color[i] = COLORS[randint(0, 5)]
+    speedx[i] = randint(-8, 8)
+    speedy[i] = randint(-8, 8)
+    lifetime[i] = randint(25, 100)
+    countertime[i] = 0
+    circle(screen, color[i], (x[i], y[i]), r[i])
+
+def update_position(i):
     global x, y, r, speedx, speedy, countertime
-    x += speedx
-    y += speedy
-    countertime += 1
-    if (countertime == lifetime):
-        new_ball()
+    x[i] += speedx
+    y[i] += speedy
+    countertime[i] += 1
+    if (countertime[i] == lifetime[i]):
+        new_ball(i)
     if (x + r > 1200):
-        speedx = randint(-8, -1)
+        speedx[i] = randint(-8, -1)
     elif (x - r < 0):
-        speedx = randint(1, 8)
+        speedx[i] = randint(1, 8)
     elif (y - r < 0):
-        speedy = randint(1, 8)
+        speedy[i] = randint(1, 8)
     elif (y + r > 900):
-        speedy = randint(-8, -1)
-    circle(screen, color, (x,y), r)
+        speedy[i] = randint(-8, -1)
+    circle(screen, color[i], (x[i],y[i]), r[i])
 
 def click(event):
-    if ((event.pos[0] - x)**2 + (event.pos[1] - y)**2) <= r**2:
-        #print("ПОПАЛ!")
+    if ((event.pos[0] - x[0])**2 + (event.pos[1] - y[0])**2) <= r[0]**2:
         add_score(5)
-        return True
+        return 0
+    elif ((event.pos[0] - x[1])**2 + (event.pos[1] - y[1])**2) <= r[1]**2:
+        add_score(5)
+        return 1
     else:
-        #print("Не попал!")
         return False
 
 def add_score(points):
@@ -66,7 +76,8 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-new_ball()
+new_ball(0)
+new_ball(1)
 
 while not finished:
     clock.tick(FPS)
@@ -74,10 +85,13 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if (click(event) == True):
-                new_ball()
+            if (click(event) == 0):
+                new_ball(0)
+            elif (click(event) == 1):
+                new_ball(1)
     screen.fill(BLACK)
-    update_position()
+    update_position(0)
+    update_position(1)
     pygame.display.update()
 
 pygame.quit()
